6ae9d36760490495acc979757fd88039aefb1965
ITFINDEN SCRIPT UTILS <50462497+itfinden@users.noreply.github.com>
2019-09-09 11:25:02 -0300

    Update itfindentnw01.php

--- modules/addons/itfindentnw01/itfindentnw01.php
+++ modules/addons/itfindentnw01/itfindentnw01.php
@@ -1,76 +1,103 @@
 <?php
+
 if (!defined("WHMCS"))
 	die("This file cannot be accessed directly");
+
 function itfindentnw01_config() {
-	$configarray = array(
-	"name" => "Itfinden Telegram Notifique Whmcs",
-	"description" => "Notifique al administrador y a los patrocinadores del sitio a través de telegram - Diseñado y codificado por Milad Livestock",
-	"version" => "1.1",
-	"author" => "ITFINDEN",
-	"language" => "english",
-	"fields" => array(
-	"key" => array ("FriendlyName" => "Bot Token", "Type" => "text", "Size" => "50", "Description" => "Túnel de arranque recibido de Telegram Robot - <a href='http://itfinden.com/telegram-bot' target='_blank' style='color:#0000FF; text-decoration: none;'>Aprende a construir telegramas y recibir robots. Bot Token</a>", "Default" => "", ),
-	"chatid" => array ("FriendlyName" => "Chat ID", "Type" => "text", "Size" => "50", "Description" => "Entra en tu chat telegrama IP - <a href='http://itfinden.com/telegram-bot' target='_blank' style='color:#0000FF; text-decoration: none;'>Recibir entrenamiento Chat ID Telegrama</a>", "Default" => "", ),
-	));
-	return $configarray;
+    $configarray = array(
+    "name" => "itfindentnw01 Telegram Notifications for WHMCS",
+    "description" => "Este huea lo cree yo",
+    "version" => "1.0",
+    "author" => "Itfinden Corp",
+    "language" => "english",
+    "fields" => array(
+        "key" => array ("FriendlyName" => "ITFINDEN API", "Type" => "text", "Size" => "50", "Description" => "", "Default" => "", ),
+    ));
+    return $configarray;
 }
+
 function itfindentnw01_activate() {
-	$query = "CREATE TABLE IF NOT EXISTS `db_itfindentnw01` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`adminid` int(11) NOT NULL,
-	`access_token` varchar(255) NOT NULL,
-	`permissions` text NOT NULL,
-	PRIMARY KEY (`id`)
-	) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;";
+  $query = "CREATE TABLE IF NOT EXISTS `itfindentnw01` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `adminid` int(11) NOT NULL,
+    `access_token` varchar(255) NOT NULL,
+    `permissions` text NOT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;";
 	$result = mysql_query($query);
 }
+
 function itfindentnw01_deactivate() {
-	$query = "DROP TABLE `db_itfindentnw01`";
+  $query = "DROP TABLE `itfindentnw01`";
 	$result = mysql_query($query);
 }
+
 function itfindentnw01_output($vars) {
-	global $customadminpath, $CONFIG;
-	$access_token = select_query('db_itfindentnw01', '', array('adminid' => $_SESSION['adminid']));
-	if ( $_GET['return'] == '1' && $_SESSION['request_token'] ) {
-		
-		insert_query("db_itfindentnw01", array("adminid" => $_SESSION['adminid'], "access_token" => $result['access_token']));
-		$_SESSION['request_token'] = "";
-		header("Location: addonmodules.php?module=itfindentnw01");
-		
-	} elseif($_GET['setup'] == '1' && !mysql_num_rows($access_token)) {
-		$_SESSION['request_token'] = $vars['key'];
-		header("Location: ". $CONFIG['SystemURL']."/".$customadminpath."/addonmodules.php?module=itfindentnw01&return=1");
-	} elseif( $_GET['disable'] == '1' && mysql_num_rows($access_token) ) {
-		full_query("DELETE FROM `db_itfindentnw01` WHERE `adminid` = '".$_SESSION['adminid']."'");
-		echo "<div class='infobox'><strong>El complemento de notificación ha sido deshabilitado con éxito.</strong>
-		<br>La base de datos del complemento de notificación se eliminó con éxito y el complemento se desactivó</div>";
-	} elseif( mysql_num_rows($access_token) && $_POST ){
-		update_query('db_itfindentnw01',array('permissions' => serialize($_POST['wt_notefication'])), array('adminid' => $_SESSION['adminid']));
-		echo "<div class='infobox'><strong>Cambios guardados</strong><br>Cambios guardados exitosamente</div>";    
-	}
-	$access_token = select_query('db_itfindentnw01', '', array('adminid' => $_SESSION['adminid']));
-	$result = mysql_fetch_array($access_token, MYSQL_ASSOC);
-	$permissions = unserialize($result['permissions']);   
-	if ( !mysql_num_rows($access_token)) {
-		echo "<p><a href='addonmodules.php?module=itfindentnw01&setup=1'>Activar el sistema para enviar notificaciones.</a></p>";
-	} else {
-		echo "<p><a href='addonmodules.php?module=itfindentnw01&disable=1'>Deshabilitar el sistema de notificación.</a></p>";
-		echo '<form method="POST"><table class="form" width="100%" border="0" cellspacing="2" cellpadding="3">
-		<tr>
-		<td class="fieldlabel" width="200px">Enviar mensaje cuando:</td>
-		<td class="fieldarea">
-		<table width="100%">
-		<tr>
-		<td valign="top">
-		<input type="checkbox" name="wt_notefication[new_client]" value="1" id="wt_notefications_new_client" '.($permissions['new_client'] == "1" ? "checked" : "").'> <label for="wt_notefications_new_client">Nuevo usuario registrado</label><br>
-		<input type="checkbox" name="wt_notefication[new_invoice]" value="1" id="wt_notefications_new_invoice" '.($permissions['new_invoice'] == "1" ? "checked" : "").'> <label for="wt_notefications_new_invoice">Factura Pagada</label><br>
-		<input type="checkbox" name="wt_notefication[new_update]" value="1" id="wt_notefications_new_update" '.($permissions['new_update'] == "1" ? "checked" : "").'> <label for="wt_notefications_new_update">El cliente respondió tickets de venta.</label><br>
-		<input type="checkbox" name="wt_notefication[new_ticket]" value="1" id="wt_notefications_new_ticket" '.($permissions['new_ticket'] == "1" ? "checked" : "").'> <label for="wt_notefications_new_ticket">Se ha creado un nuevo ticker de soporte.</label><br>
-        <input type="checkbox" name="wt_notefication[new_logon]" value="1" id="wt_notefications_new_logon" '.($permissions['new_logon'] == "1" ? "checked" : "").'> <label for="wt_notefications_new_logon">Se ha iniciado logon como Administrador.</label><br>		
-		</td>
-		</tr>
-		</table>
-		</table>
-		<p align="center"><input type="submit" value="Guardar Cambios" class="button"></p></form>';
-	}
+  global $customadminpath, $CONFIG;
+  
+  $access_token = select_query('itfindentnw01', '', array('adminid' => $_SESSION['adminid']));
+ 
+  if ( $_GET['return'] == '1' && $_SESSION['request_token'] ) {    
+    
+    $response = curlCall("https://notiapp.com/api/v1/get_access_token",array('app' => $vars['key'], 'request_token' => $_SESSION['request_token']));
+    
+    $result = json_decode($response, true);    
+    insert_query("itfindentnw01", array("adminid" => $_SESSION['adminid'], "access_token" => $result['access_token']) );
+    $_SESSION['request_token'] = "";
+    curlCall("https://notiapp.com/api/v1/add",array('app' => $vars['key'], 'user' => $result['access_token'], "notification[title]" => "WHMCS is ready to go!", "notification[text]" => "You will now receive WHMCS notifications directly to your desktop", "notification[sound]" => "alert1"));
+    header("Location: addonmodules.php?module=itfindentnw01");
+    
+  } elseif($_GET['setup'] == '1' && !mysql_num_rows($access_token)) {
+   
+    $response = curlCall("https://notiapp.com/api/v1/request_access",array('app' => $vars['key'], 'redirect_url' => $CONFIG['SystemURL']."/".$customadminpath."/addonmodules.php?module=itfindentnw01&return=1"));
+    $result = json_decode($response, true);
+    
+  
+    if ($result['request_token'] && $result['redirect_url']) {
+      $_SESSION['request_token'] = $result['request_token'];
+      header("Location: ".$result['redirect_url']);
+      
+    } else {
+      echo "<div class='errorbox'><strong>Incorrect API Key</strong></br>Incorrect ITFINDEN API Key specified.  </div>";
+    }
+  } elseif( $_GET['disable'] == '1' && mysql_num_rows($access_token) ) {
+    full_query("DELETE FROM `itfindentnw01` WHERE `adminid` = '".$_SESSION['adminid']."'");
+    echo "<div class='infobox'><strong>Successfully Disabled itfindentnw01</strong></br>You have successfully disabled itfindentnw01.</div>";
+  } elseif( mysql_num_rows($access_token) && $_POST ){
+    update_query('itfindentnw01',array('permissions' => serialize($_POST['notification'])), array('adminid' => $_SESSION['adminid']));
+    echo "<div class='infobox'><strong>Updated Notifications</strong></br>You have successfully updated your notification preferences.</div>";    
+  }
+  
+  $access_token = select_query('itfindentnw01', '', array('adminid' => $_SESSION['adminid']));
+  $result = mysql_fetch_array($access_token, MYSQL_ASSOC);
+  $permissions = unserialize($result['permissions']);   
+
+  if ( !mysql_num_rows($access_token)) {
+    echo "<p><a href='addonmodules.php?module=itfindentnw01&setup=1'>Setup itfindentnw01</a></p>";
+  } else {
+    echo "<p><a href='addonmodules.php?module=itfindentnw01&disable=1'>Disable itfindentnw01</a></p>";
+    
+    echo '<form method="POST"><table class="form" width="100%" border="0" cellspacing="2" cellpadding="3">
+    <tr>
+      <td class="fieldlabel" width="200px">Notifications</td>
+      <td class="fieldarea">
+      <table width="100%">
+        <tr>
+           <td valign="top">
+             <input type="checkbox" name="notification[new_client]" value="1" id="notifications_new_client" '.($permissions['new_client'] == "1" ? "checked" : "").'> <label for="notifications_new_client">New Clients</label><br>
+             <input type="checkbox" name="notification[new_invoice]" value="1" id="notifications_new_invoice" '.($permissions['new_invoice'] == "1" ? "checked" : "").'> <label for="notifications_new_invoice">Paid Invoices</label><br>
+             <input type="checkbox" name="notification[new_ticket]" value="1" id="notifications_new_ticket" '.($permissions['new_ticket'] == "1" ? "checked" : "").'> <label for="notifications_new_ticket">New Support Ticket</label><br>
+             <input type="checkbox" name="notification[new_update]" value="1" id="notifications_new_update" '.($permissions['new_update'] == "1" ? "checked" : "").'> <label for="notifications_new_update">New Support Ticket Update</label><br>
+           </td>
+         </tr>
+         
+    </table>
+  </table>
+  
+  <p align="center"><input type="submit" value="Save Changes" class="button"></p></form>
+  ';
+  }
+  
+  
+
 }
