b28307262a72738c8c84b2bd424d81a7b7a709c7
ITFINDEN SCRIPT UTILS <50462497+itfinden@users.noreply.github.com>
2019-09-09 11:36:37 -0300

    Update itfindentnw01.php

--- modules/addons/itfindentnw01/itfindentnw01.php
+++ modules/addons/itfindentnw01/itfindentnw01.php
@@ -54,18 +54,19 @@ function itfindentnw01_output($vars) {
     
   
     if ($result['request_token'] && $result['redirect_url']) {
+        
       $_SESSION['request_token'] = $result['request_token'];
       header("Location: ".$result['redirect_url']);
       
     } else {
-      echo "<div class='errorbox'><strong>Incorrect API Key</strong></br>Incorrect ITFINDEN API Key specified.  </div>";
+      echo "<div class='errorbox'><strong>Incorrecta API Key</strong></br>Incorrecta ITFINDEN API Key Informada.  </div>";
     }
   } elseif( $_GET['disable'] == '1' && mysql_num_rows($access_token) ) {
     full_query("DELETE FROM `itfindentnw01` WHERE `adminid` = '".$_SESSION['adminid']."'");
     echo "<div class='infobox'><strong>Successfully Disabled itfindentnw01</strong></br>You have successfully disabled itfindentnw01.</div>";
   } elseif( mysql_num_rows($access_token) && $_POST ){
     update_query('itfindentnw01',array('permissions' => serialize($_POST['notification'])), array('adminid' => $_SESSION['adminid']));
-    echo "<div class='infobox'><strong>Updated Notifications</strong></br>You have successfully updated your notification preferences.</div>";    
+    echo "<div class='infobox'><strong>Se ha Actualizado itfindentnw01</strong></br>Se logro actualizar la Informacion.</div>";    
   }
   
   $access_token = select_query('itfindentnw01', '', array('adminid' => $_SESSION['adminid']));
@@ -73,28 +74,28 @@ function itfindentnw01_output($vars) {
   $permissions = unserialize($result['permissions']);   
 
   if ( !mysql_num_rows($access_token)) {
-    echo "<p><a href='addonmodules.php?module=itfindentnw01&setup=1'>Setup itfindentnw01</a></p>";
+    echo "<p><a href='addonmodules.php?module=itfindentnw01&setup=1'>Configurar itfindentnw01</a></p>";
   } else {
-    echo "<p><a href='addonmodules.php?module=itfindentnw01&disable=1'>Disable itfindentnw01</a></p>";
+    echo "<p><a href='addonmodules.php?module=itfindentnw01&disable=1'>Desabilitar itfindentnw01</a></p>";
     
     echo '<form method="POST"><table class="form" width="100%" border="0" cellspacing="2" cellpadding="3">
     <tr>
-      <td class="fieldlabel" width="200px">Notifications</td>
+      <td class="fieldlabel" width="200px">Notificaciones</td>
       <td class="fieldarea">
       <table width="100%">
         <tr>
            <td valign="top">
-             <input type="checkbox" name="notification[new_client]" value="1" id="notifications_new_client" '.($permissions['new_client'] == "1" ? "checked" : "").'> <label for="notifications_new_client">New Clients</label><br>
-             <input type="checkbox" name="notification[new_invoice]" value="1" id="notifications_new_invoice" '.($permissions['new_invoice'] == "1" ? "checked" : "").'> <label for="notifications_new_invoice">Paid Invoices</label><br>
-             <input type="checkbox" name="notification[new_ticket]" value="1" id="notifications_new_ticket" '.($permissions['new_ticket'] == "1" ? "checked" : "").'> <label for="notifications_new_ticket">New Support Ticket</label><br>
-             <input type="checkbox" name="notification[new_update]" value="1" id="notifications_new_update" '.($permissions['new_update'] == "1" ? "checked" : "").'> <label for="notifications_new_update">New Support Ticket Update</label><br>
+             <input type="checkbox" name="notification[new_client]" value="1" id="notifications_new_client" '.($permissions['new_client'] == "1" ? "checked" : "").'> <label for="notifications_new_client">Nuevos Clientes</label><br>
+             <input type="checkbox" name="notification[new_invoice]" value="1" id="notifications_new_invoice" '.($permissions['new_invoice'] == "1" ? "checked" : "").'> <label for="notifications_new_invoice">Pagar Facturas</label><br>
+             <input type="checkbox" name="notification[new_ticket]" value="1" id="notifications_new_ticket" '.($permissions['new_ticket'] == "1" ? "checked" : "").'> <label for="notifications_new_ticket">Nuevo Ticket</label><br>
+             <input type="checkbox" name="notification[new_update]" value="1" id="notifications_new_update" '.($permissions['new_update'] == "1" ? "checked" : "").'> <label for="notifications_new_update">Nuevo Tickets Respuestas</label><br>
            </td>
          </tr>
          
     </table>
   </table>
   
-  <p align="center"><input type="submit" value="Save Changes" class="button"></p></form>
+  <p align="center"><input type="submit" value="Grabar Cambios" class="button"></p></form>
   ';
   }
   
