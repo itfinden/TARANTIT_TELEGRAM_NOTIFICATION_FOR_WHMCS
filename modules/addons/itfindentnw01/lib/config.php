d3c8b3cc9a885a9865c8efc0f9a431dd57e8e975
Man Of Orion <cm@itfinden.com>
2019-09-11 12:17:03 -0300

    carga

---
+++ modules/addons/itfindentnw01/lib/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$GLOBALS['telegram_bot'] = "364508222:AAH4U4SRhuhk2bKJSlznSwIfAEPQtrVXAH0";
+$GLOBALS['telegram_chat'] = "-360580051";
+#$GLOBALS['telegram_date'] = date(DateTime::ISO8601);
+$GLOBALS['telegram_date'] = date("d-m-Y H:i:s");
+$GLOBALS['telegram_url'] = "https://comunica.itfinden.com/commands/process";
+$GLOBALS['whmcsAdminURL'] = "https://customer.itfinden.com/gestion/";
+$GLOBALS['companyName'] = "ITFINDEN CORP";
+$GLOBALS['discordColor'] = hexdec("");
+$GLOBALS['logo'] = "";
+
+?>
